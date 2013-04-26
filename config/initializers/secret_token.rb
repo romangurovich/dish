5e8ca5e18ca6528e1403c52968ce783405aad7b9
Roman Gurovich <romangurovich@gmail.com>
2013-04-26 10:51:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dish::Application.config.secret_token = '67b9220f1d4f49cbd3bcd9d825229394f483c5f8e9d4b856012b26f9ba42c6cc449504535681588203976c8c16635f0bbfefce43df4445b5714a9d221ece9e5e'
